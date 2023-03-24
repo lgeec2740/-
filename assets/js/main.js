fba593d0d89bd152c48d3d56a5e500725fbed648
lgeec2740 <dmitriymachtak@gmail.com>
2023-03-24 20:39:56 +1000

    Syte

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,115 +1,64 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+//Подключим наши страницы
+import all from "../views/Page1"
+import Page2 from "../views/Page2"
+import Page3 from "../views/Page3"
+import Page4 from "../views/Page4"
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Page1
+        //Так, как синий шаблон у нас является дефолтным, его можно не указывать в мета-данных
+    },
+    {
+        path: '/page2',
+        name: 'Page2',
+        component: Page2,
+        //А вот это свойство как раз будет содержать название компонента-шаблона,
+        //который мы хотим использовать для данной страницы
+        meta:{
+            layout: "green-layout"
+        }
+    },
+    {
+        path: '/page3',
+        name: 'Page3',
+        component: Page3,
+        meta:{
+            layout: "red-layout"
+        }
+    },
+    {
+        path: '/page4',
+        name: 'Page4',
+        component: Page4,
+        //И снова ничего не указываем, чтобы задействовать дефолтный шаблон
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
+
 var app = new Vue({
     el: '#app',
-    name:'App',
-    data(){return {
-        isMain: true,
-        isAll: false,
-        isCreate: false, 
-        isEdit: false, 
-        isManage: false, 
-        isPost: false, 
-      }
+    data: {
+        items:[
+           { id: "1"}
+        ]
     },
-    data(){return {
-        isMain: false,
-        isAll: true,
-        isCreate: false, 
-        isEdit: false, 
-        isManage: false, 
-        isPost: false, 
-      }
-    },
-    data(){return {
-        isMain: false,
-        isAll: false,
-        isCreate: true, 
-        isEdit: false, 
-        isManage: false, 
-        isPost: false, 
-      }
-    },
-    data(){return {
-        isMain: false,
-        isAll: false,
-        isCreate: false, 
-        isEdit: true, 
-        isManage: false, 
-        isPost: false, 
-      }
-    },
-    data(){return {
-        isMain: false,
-        isAll: false,
-        isCreate: false, 
-        isEdit: false, 
-        isManage: true, 
-        isPost: false, 
-      }
-    },
-    data(){return {
-        isMain: false,
-        isAll: false,
-        isCreate: false, 
-        isEdit: false, 
-        isManage: false, 
-        isPost: true, 
-      }
-    },
-    methods: {
-      enterMain() {
-        this.isMain = true;
-        this.isAll = false;
-        this.isCreate = false;
-        this.isEdit = false;
-        this.isManage = false;
-        this.isPost = false;
-        this.$forceUpdate();
-      },
-      enterAll() {
-        this.isMain = false;
-        this.isAll = true;
-        this.isCreate = false;
-        this.isEdit = false;
-        this.isManage = false;
-        this.isPost = false;
-        this.$forceUpdate();
-      },
-      enterCreate() {
-        this.isMain = false;
-        this.isAll = false;
-        this.isCreate = true;
-        this.isEdit = false;
-        this.isManage = false;
-        this.isPost = false;
-        this.$forceUpdate();
-      },
-      enterEdit() {
-        this.isMain = false;
-        this.isAll = false;
-        this.isCreate = false;
-        this.isEdit = true;
-        this.isManage = false;
-        this.isPost = false;
-        this.$forceUpdate();
-      },
-      enterManage() {
-        this.isMain = false;
-        this.isAll = false;
-        this.isCreate = false;
-        this.isEdit = false;
-        this.isManage = true;
-        this.isPost = false;
-        this.$forceUpdate();
-      },
-      enterPost() {
-        this.isMain = false;
-        this.isAll = false;
-        this.isCreate = false;
-        this.isEdit = false;
-        this.isManage = false;
-        this.isPost = true;
-        this.$forceUpdate();
-      }
+    methods:{
+        deleteItem: function(index) {
+            this.items.splice(index, 1);
+        }
     }
-    
 })
