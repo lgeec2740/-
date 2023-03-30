a7c6e5dbc50c59342ec87e5396c6701c1a8b860c
lgeec2740 <dmitriymachtak@gmail.com>
2023-03-30 17:15:31 +1000

    Syte2

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,10 +45,46 @@ const All = Vue.component('All', {template:'<div class="container"><section clas
   }
 })
 
+const Create = Vue.component('Create', {template:'<div class="container"> <section class="py-5"> <div class="mb-4 d-flex justify-content-between align-items-center"> <h2>добавление статьи</h2> </div>  <form>  <div class="mb-3"> <label>название</label> <input type="text" class="form-control" placeholder="придумайте название статьи"> </div> <div class="mb-3"> <label>описание</label> <textarea class="form-control" placeholder="напишите описание статьи" rows="5"></textarea> </div> <div class="mb-4"> <label>изображение</label> <input type="file" class="form-control">  <img src="assets/images/placeholder-blue.png" class="w-100 mt-4" alt="preview"> </div>  <button class="btn btn-success">добавить статью</button> </form> </section> </div>',
+   data(){
+    return{
+
+    }
+   }
+})
+
+const Edit = Vue.component('Edit',{template:'<div class="container"> <section class="py-5"> <div class="mb-4 d-flex justify-content-between align-items-center"> <h2>изменение статьи</h2> </div>  <form>  <div class="mb-3"> <label>название</label> <input type="text" class="form-control" placeholder="придумайте название статьи"> </div> <div class="mb-3"> <label>описание</label> <textarea class="form-control" placeholder="напишите описание статьи" rows="5"></textarea> </div> <div class="mb-4"> <label>изображение</label> <input type="file" class="form-control">  <img src="assets/images/placeholder-blue.png" class="w-100 mt-4" alt="preview"> </div>  <button class="btn btn-success">сохранить</button> </form> </section> </div>',
+  data(){
+    return{
+
+    }
+  }
+})
+
+const Manage = Vue.component('Manage',{template:'<div class="container"> <section class="py-5"> <div class="mb-4 d-flex justify-content-between align-items-center"> <h2>управление статьями</h2> <button class="btn btn-sm btn-success" to="/create">добавить статью</button> </div>  <table class="table" v-for="(item, index) in articles" :key="index"> <tr> <th>#</th> <th>название</th> <th>дата и время</th> <th>действия</th> </tr> <tr> <td>{{item.id}}</td> <td>{{item.name}}</td> <td>{{item.date}}</td> <td> <button class="btn btn-primary btn-sm">редактировать</button> <button class="btn btn-danger btn-sm" v-on:click="deleteitem(index)">удалить</button> </td> </tr> </table> </section> </div>',
+  data(){
+    return{
+
+    }
+  }
+})
+
+const Post = Vue.component('Post',{template:'<div class="container"> <section class="py-5"> <h2 class="mb-4">название статьи</h2>  <img class="mb-4 w-100" src="assets/images/placeholder-blue.png" alt="placeholder">  <p> lorem ipsum dolor sit amet, consectetur adipisicing elit. cum dignissimos sequi similique. asperiores atque autem cumque eius expedita id illo incidunt laborum libero magni neque nostrum nulla quaerat quam quas repellat repudiandae sed soluta, totam veniam. accusantium at commodi consequatur, dolore est facere fugiat maiores modi, nemo perspiciatis placeat, quae quaerat quas quibusdam quos reiciendis rem sunt tenetur ut voluptatibus. aspernatur, autem blanditiis, commodi consequuntur culpa cumque, deserunt dignissimos eius expedita illo ipsam iste officiis quam quasi qui quidem recusandae sint soluta. aliquam aliquid consectetur dolores id? animi, aut doloremque error illo labore laudantium, maxime molestias nesciunt porro, sunt ut! </p> </section> </div>',
+data(){
+  return{
+
+  }
+}
+})
 
 const routes = [
     { path: '/', component: Main },
     { path: '/all', component: All },
+    { path: '/create', component: Create },
+    { path: '/edit', component: Edit },
+    { path: '/manage', component: Manage },
+    { path: '/post', component: Post }
+ 
   ]
   const router = new VueRouter({
     routes // сокращённая запись для routes: routes
